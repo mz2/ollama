71c57255d4f1ec47ff6fc7c4cf5c302da5dd6375
Matias Piipari <matias.piipari@gmail.com>
2024-04-20 21:59:06 +0300

    Add support for rocm (?) and compiling in cpu_avx and cpu_avx2, add support for configuring the LLM library to use.

--- snap/local/snap_launcher.sh
+++ snap/local/snap_launcher.sh
@@ -4,10 +4,15 @@ set -e
 OLLAMA_HOST=$(snapctl get host)
 OLLAMA_MODELS=$(snapctl get models)
 OLLAMA_ORIGINS=$(snapctl get origins)
-CUDA_VISIBLE_DEVICES_VALUE=$(snapctl get cuda-visible-devices)
 
+CUDA_VISIBLE_DEVICES_VALUE=$(snapctl get cuda-visible-devices)
 if [ -n "$CUDA_VISIBLE_DEVICES_VALUE" ]; then
     export CUDA_VISIBLE_DEVICES=$CUDA_VISIBLE_DEVICES_VALUE
+fi
+
+OLLAMA_LLM_LIBRARY=$(snapctl get ollama-llm-library)
+if [ -n "$OLLAMA_LLM_LIBRARY" ]; then
+    export OLLAMA_LLM_LIBRARY
 fi
 
 export OLLAMA_HOST OLLAMA_MODELS OLLAMA_ORIGINS
